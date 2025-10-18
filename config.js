463eeec2fe1e31f886c81bc08a1cd1f2a8505e79
PranaySAG <pranayshinde929@gmail.com>
2025-10-18 21:28:55 +0530

    server created

---
+++ config.js
@@ -0,0 +1,17 @@
+export const TODO_ITEMS = [
+    {
+        id: 1,
+        title: "Finish project",
+        completed: false,
+        priority: "high",
+        emoji: "😶"
+    },
+    {
+        id: 2,
+        title: "Prepare breakfast",
+        completed: true,
+        priority: "medium",
+        emoji: "🍳"
+    },
+    
+];
